e067b7eb9c6e84579078b997cc3952478048fd6f
Ahmed Essam Mohamed <aessam72@gmail.com>
2018-10-27 14:07:39 +0200

    Update Execution.c

--- Execution.c
+++ Execution.c
@@ -7,41 +7,50 @@
 #include <unistd.h>
 #include "handlers.h"
 
+/*Background flag referenced from Parser.c*/
 extern int background;
 
 int main(void) {
-        void (*handler)(int);
-        handler = SET_HANDLER();
-        /*Terminal Command*/
-        char** args =(char**)malloc(50);
-        /*Initialize Array of strings that keep the parsed command*/
-        char** parsed=(char**)malloc(50);
-        args[0] = (char *)malloc(50);
+    /*Initializing Call Back Function*/
+    void (*handler)(int);
+    handler = SET_HANDLER();
+    /*Setting the handler of SIGCHLD signal*/
+    signal(SIGCHLD,handler);
+    /*Terminal Command*/
+    char** args =(char**)malloc(50);
+    /*Initialize Array of strings that keep the parsed command*/
+    char** parsed=(char**)malloc(50);
+    args[0] = (char *)malloc(50);
     while (1) {
+	printf("User:$ ");
+	/*Scanning the command*/
         scanf("%[^\n]%*c", args[0]);
+	/*Parsing the command*/
         parsed = args_parser(args);
+	/*Forking a process*/
         pid_t pid = fork();
-        if (pid == 0)
+	/*CHILD*/        
+	if (pid == 0)
         {
-            /*printf("CHILD\n");*/
             if (execvp(parsed[0], parsed) == -1) {
                 _exit(-1);
             }
         }
+	/*PARENT*/
         else if (pid > 0)
         {
-            signal(SIGCHLD,handler);
+	    /*If the command is to run in foreground*/
             if(!background) {
+		/*The Parent Waits the child to finish*/
                 wait(0);
                 background = 0;
             }
+    	    /*If the command is Exit*/
             if(strcmp(parsed[0],"exit")==0)
             {
                 exit(0);
             }
-            /*printf("PARENT\n");*/
         }
     }
     return 0;
 }
-
